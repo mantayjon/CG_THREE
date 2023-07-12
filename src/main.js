4d79857c9a3daf8ce2af4943a6fddfd23e96c78a
mantayjon <Jonas.Mantay@Student.HTW-Berlin.de>
2023-07-12 12:19:52 +0200

    center text

--- src/main.js
+++ src/main.js
@@ -1,18 +1,9 @@
 import { World } from './World/World.js';
 
 function main() {
-  // Get a reference to the container element
+
   const container = document.querySelector('#scene-container');
-
-  // 1. Create an instance of the World app
   const world = new World(container);
-
-  const startButton = document.querySelector('#startButton');
-
-  startButton.addEventListener('click', function() {
-    // Render the scene when the button is clicked
-    
-  });
   
   world.render();
 }
